0fd2f5436d9e590f7990c82291e1aecee47d4884
verilucena <veridianalucena@gmail.com>
2021-05-17 14:32:32 -0300

    admin registers courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -8,8 +8,13 @@ class CoursesController < ApplicationController
     end
 
     def new
-        @courses = Course.new(params.require(:course).permit(:name, :description, :code, :price, :enrollment_deadline))
-        @courses.save
+        @course = Course.new
     end
 
+    def create 
+        @course = Course.new(params.require(:course).permit(:name, :description, :code, :price, :enrollment_deadline))
+        if @course.save
+            redirect_to @course
+        end
+    end
 end
